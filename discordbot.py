1af35eceb6cd0787c3f5b20279f20a885b36f3ed
gx1285 <78240988+gx1285@users.noreply.github.com>
2021-10-08 17:13:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -72,7 +72,7 @@ async def help(ctx):
     
 @bot.command()
 async def report(ctx, *, value):
-    channel = guild.get_channel(895268495745507348)
+    channel = bot.get_channel(895268495745507348)
     await channel.send(f"{str(value)} - {ctx.author.name}|{ctx.author.id}")
     await ctx.add_reaction('✅')
     await ctx.send("送信しました。")
