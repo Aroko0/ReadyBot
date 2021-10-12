870d0906e364f653ae645dc9355a38b4a2537c6d
gx1285 <78240988+gx1285@users.noreply.github.com>
2021-10-12 19:04:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -70,7 +70,42 @@ async def help(ctx):
     embed.set_footer(text=f" 実行者: {ctx.author} ", icon_url=ctx.author.avatar_url)
     await ctx.send(embed=embed)
 
+@bot.listen()
+async def on_message(message):
+    if message.channel.name == global_channel_name:
+        if message.author.bot:
+            return
+        for channel in bot.get_all_channels(): 
+            if channel.name == global_channel_name: 
+                if channel == message.channel: 
+                    continue
 
+                embed=discord.Embed(description=message.content, color=discord.Colour.dark_blue()) 
+                embed.set_author(name="{}#{}".format(message.author.name, message.author.discriminator),icon_url="https://media.discordapp.net/avatars/{}/{}.png?size=1024".format(message.author.id, message.author.avatar))
+                embed.set_footer(text="{} / メッセージid {}".format(message.guild.name, message.id),icon_url="https://media.discordapp.net/icons/{}/{}.png?size=1024".format(message.guild.id, message.guild.icon))
+                if message.attachments != []: #添付ファイルが存在するとき
+                    embed.set_image(url=message.attachments[0].url)
+
+                if message.reference:
+                    reference_msg = await message.channel.fetch_message(message.reference.message_id) 
+                    if reference_msg.embeds and reference_msg.author == bot.user: 
+                        reference_message_content = reference_msg.embeds[0].description 
+                        reference_message_author = reference_msg.embeds[0].author.name 
+                    elif reference_msg.author != bot.user:
+                        reference_message_content = reference_msg.content 
+                        reference_message_author = reference_msg.author.name+'#'+reference_msg.author.discriminator
+                    reference_content = ""
+                    for string in reference_message_content.splitlines(): 
+                        reference_content += "> " + string + "\n"
+                    reference_value = "**@{}**\n{}".format(reference_message_author, reference_content) 
+                    embed.add_field(name='返信', value=reference_value, inline=True) 
+                await channel.send(embed=embed)
+
+@bot.event
+async def on_message_edit(before, after):
+            if after.author.bot: 
+                return    
+            await after.edit(after.id)
 
 @bot.command()
 async def report(ctx, *, value):
